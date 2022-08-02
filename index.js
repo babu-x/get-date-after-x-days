1564b2c8d744398b62e6819244c7bb427add865a
babu-x <bhaskar170863@gmail.com>
2022-08-02 06:43:43 +0000

    Test case failed: the function should return the days after x days

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const addDays = require('date-fns/addDays');
-const result = addDays(new Date(2020,7,22),10);
+const format = require('date-fns/format');
+
+let result = (x) => {
+    let days = addDays(new Date(2020,7,22), x);
+    let formatted = format(days, 'dd-MM-yyyy');
+    return formatted;
+}
+
+
 
 module.exports = result;
